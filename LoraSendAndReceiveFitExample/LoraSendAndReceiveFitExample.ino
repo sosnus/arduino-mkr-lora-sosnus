1115a8368ab4ec993f243f36084ff69122b05d08
Sosnus <sosnus15@gmail.com>
2019-11-10 21:21:59 +0100

    upgradefit example

--- LoraSendAndReceiveFitExample/LoraSendAndReceiveFitExample.ino
+++ LoraSendAndReceiveFitExample/LoraSendAndReceiveFitExample.ino
@@ -1,99 +1,71 @@
-/*
-  Lora Send And Receive
-  This sketch demonstrates how to send and receive data with the MKR WAN 1300 LoRa module.
-  This example code is in the public domain.
-*/
-
-//  Your device EUI is: a8610a3337386b0c
-//const char *appEui = "70B3D57ED0024800";
-//const char *appKey = "1983ED48628325F566503F3D7541FDED";
-
 #include <Arduino.h>
 #include "wiring_private.h"
-Uart mySerial1 (&sercom3, 0, 1, SERCOM_RX_PAD_1, UART_TX_PAD_0); // Create the new UART instance assigning it to pin 0 and 1
-
-
-
+#include "arduino_secrets.h"
 #include <MKRWAN.h>
 
+Uart mySerial1(&sercom3, 0, 1, SERCOM_RX_PAD_1, UART_TX_PAD_0); // Create the new UART instance assigning it to pin 0 and 1
 LoRaModem modem;
 
-#include "arduino_secrets.h"
-// Please enter your sensitive data in the Secret tab or arduino_secrets.h
 String appEui = SECRET_APP_EUI;
 String appKey = SECRET_APP_KEY;
 
-void setup() {
-    mySerial1.begin(9600);
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  
+  // DEBUG INIT
+  mySerial1.begin(9600);
   pinPeripheral(0, PIO_SERCOM); //Assign RX function to pin 0
   pinPeripheral(1, PIO_SERCOM); //Assign TX function to pin 1
-    mySerial1.println("LORA FitExample");
+  mySerial1.println("LORA FitExample");
 
-  if (!modem.begin(EU868)) {
+  // LORA INIT
+  if (!modem.begin(EU868))
+  {
     mySerial1.println("[ERR] Failed to start module");
-    while (1) {}
+    while (1);
   };
-  delay(200);
-  mySerial1.println("Your device EUI is: ");
-  mySerial1.println(modem.deviceEUI());
-  delay(200);
 
+  // LORA CONNECT
   int connected = modem.joinOTAA(appEui, appKey);
-  if (!connected) {
+  if (!connected)
+  {
     mySerial1.println("[ERR] Can't get GW");
-    while (1) {}
+    while (1);
   }
 
-  modem.minPollInterval(60); //rm in future?
   mySerial1.println("SETUP END");
+  digitalWrite(LED_BUILTIN, LOW);
 }
-int  myCounter = 1;
-void loop() {
-  mySerial1.println();
-  delay(200);
 
-  // while (!Serial.available());
-  String msg = String(myCounter++);    //Serial.readStringUntil('\n');
-  mySerial1.println("New msg: " + myCounter);
-  delay(200);
+int myCounter = 1;
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  mySerial1.println();
+  String msg = String(myCounter++);
 
-  mySerial1.println();
-  mySerial1.print("Sending: " + msg + " - ");
-  for (unsigned int i = 0; i < msg.length(); i++) {
-    mySerial1.print(msg[i] >> 4, HEX);
-    mySerial1.print(msg[i] & 0xF, HEX);
-    mySerial1.print(" ");
-  }
-  mySerial1.println();
-  delay(200);
+  mySerial1.println("Sending msg: <" + msg + ">");
 
   int err;
   modem.beginPacket();
   modem.print(msg);
+  modem.endPacket(false);
+  /*
   err = modem.endPacket(true);
-  if (err > 0) {
+  if (err > 0)
+  {
     mySerial1.println("[OK] SENT!");
-  } else {
-    mySerial1.println("[ERR] Cant send limit?");
   }
-  delay(1000);
-  if (!modem.available()) {
-    mySerial1.println("[DOWNLINK] NO");
-    return;
+  else
+  {
+    mySerial1.println("[ERR] No respond from GW");
   }
-  char rcv[64];
-  int i = 0;
-  while (modem.available()) {
-    rcv[i++] = (char)modem.read();
-  }
-    mySerial1.println("[DOWNLINK] YES");
-  // mySerial1.print("RECEIVED!!: ");
-  for (unsigned int j = 0; j < i; j++) {
-    mySerial1.print(rcv[j] >> 4, HEX);
-    mySerial1.print(rcv[j] & 0xF, HEX);
-    mySerial1.print(" ");
-  }
-  mySerial1.println();
+  */
+  digitalWrite(LED_BUILTIN, LOW);
+
+  delay(10000);
 }
 
 // Attach the interrupt handler to the SERCOM
